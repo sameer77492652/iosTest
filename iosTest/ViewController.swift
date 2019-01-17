1db372525c60c7f631d14478bcc667b957b32454
Sud <sudhanshus@sudhanshus-mac-mini.local>
2019-01-17 16:10:59 +0530

    testsudtest

--- iosTest/ViewController.swift
+++ iosTest/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("shikha")
+        print("sud")
         // Do any additional setup after loading the view, typically from a nib.
     }
 }
